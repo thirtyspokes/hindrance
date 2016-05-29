2f1fb3f08d790268faf470928898e56a99d04d4b
Ray Ashman Jr <ray@rayashmanjr.com>
2016-05-29 15:33:19 -0400

    Version bump

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject thirtyspokes/hindrance "0.1.3"
+(defproject thirtyspokes/hindrance "0.1.4"
   :description "A convenience wrapper for using OAuth JWT credentials flow with clj-http."
   :url "https://github.com/thirtyspokes/hindrance"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.0.1"]
                  [environ "1.0.2"]
                  [clj-jwt "0.1.1"]
